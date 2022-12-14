2f5e45daec923285396c552940a20ae9d761dc96
Vuong <vuongqd@minhhoangjsc.com>
2022-12-14 16:55:08 +0700

    WXVL-7 | Update Admin Database

--- models/user.js
+++ models/user.js
@@ -6,7 +6,6 @@ const userSchema = new mongoose.Schema({
     },
     phone:{
         type:String,
-        trim: true
     },
     roll:{
         type:String,
@@ -17,30 +16,32 @@ const userSchema = new mongoose.Schema({
     password:{
         type:String,
     }
+}, {
+    timestamps: true,
 })
-userSchema.methods = {
-    authenticate(password){
-        try {
-            return this.password == this.encrytPassword(password)
-        } catch (error) {
-            console.log(error);
-        }
-    },
-    encrytPassword(password){
-        if(!password) return
-        try {
-            return createHmac('sha256','123456').update(password).digest('hex')
-        } catch (error) {
-            console.log(error);
-        }
-    }
-}
-userSchema.pre("save",function(next){
-    try {
-        this.password = this.encrytPassword(this.password)
-        next()
-    } catch (error) {
-        console.log(error);
-    }
-})
+// userSchema.methods = {
+//     authenticate(password){
+//         try {
+//             return this.password == this.encrytPassword(password)
+//         } catch (error) {
+//             console.log(error);
+//         }
+//     },
+//     encrytPassword(password){
+//         if(!password) return
+//         try {
+//             return createHmac('sha256','123456').update(password).digest('hex')
+//         } catch (error) {
+//             console.log(error);
+//         }
+//     }
+// }
+// userSchema.pre("save",function(next){
+//     try {
+//         this.password = this.encrytPassword(this.password)
+//         next()
+//     } catch (error) {
+//         console.log(error);
+//     }
+// })
 export default mongoose.model('User',userSchema)
